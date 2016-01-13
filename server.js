fef46aa1b57c9bd6958d7e8d1d61a4e803284849
khangai <khangai@alchemist.mn>
2016-01-13 13:57:18 +0800

    i

--- server.js
+++ server.js
@@ -467,9 +467,9 @@ io.sockets.on("connection", function (socket) {
 					console.log(alice_secret);
 					// We should now have the same shared secret. 
 					console.log(alice_secret.length);
-					//if(alice_secret === bob_secret){
-					io.sockets.in(socket.room).emit("chat", msTime, people[socket.id], msg,0);
-					//}
+					if(alice_secret === bob_secret){
+						io.sockets.in(socket.room).emit("chat", msTime, people[socket.id], msg,0);
+					}
 				}
 				
 				socket.emit("isTyping", false);
@@ -547,7 +547,31 @@ io.sockets.on("connection", function (socket) {
 				}
 
 				else{
-					io.sockets.in(socket.room).emit("private_chat", msTime, people[socket.id], msg, 0);
+					var password = msg;
+ 
+					// Prepares an object for both Alice and Bob, with a prime value set. 
+					var alice = SPEKE.getSPEKE('modp5');
+					var bob = SPEKE.getSPEKE('modp5');
+					 
+					// Initialize the generator, based on the password, as well as create the 
+					// public and private keys. 
+					alice.generateKeys(password);
+					bob.generateKeys(password);
+					 
+					// Compute the shared secret, with Alice using Bob's public key, and Bob using 
+					// Alice's public key. 
+					var alice_secret = alice.computeSecret(bob.getPublicKey(), null, 'hex');
+					var bob_secret = bob.computeSecret(alice.getPublicKey(), null, 'hex');
+					console.log("Private msg in Deffie Hellman public key");
+					console.log(alice.getPublicKey());
+					console.log("Private room msg in Deffie Hellman secret key");
+					console.log(alice_secret);
+					// We should now have the same shared secret. 
+					console.log(alice_secret.length);
+					if(alice_secret === bob_secret){
+						io.sockets.in(socket.room).emit("private_chat", msTime, people[socket.id], msg, 0);
+					}
+					
 				}
 				
 				socket.emit("isTyping", false);
