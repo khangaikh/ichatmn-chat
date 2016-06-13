b223070536dcd45eb7bc71cab388b46e8abb48e5
khangaikh <khangai@alchemist.mn>
2016-06-13 13:22:28 +0900

    Some change

--- server.js
+++ server.js
@@ -334,7 +334,9 @@ io.sockets.on("connection", function (socket) {
 
 					var keySizeBits = 1024;
 					var keyPair = ursa.generatePrivateKey(keySizeBits, 65537);
-					crt = ursa.createPublicKey(keySizeBits, 65537);
+					var pem =toPublicPem(keyPair,"sha256");
+
+					crt = ursa.createPublicKey(pem, "sha256");
 					
 					console.log('Encrypt with Public');
 
